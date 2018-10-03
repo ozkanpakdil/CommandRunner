cee18805561312b967ac49f98ee16b8f11b3555f
ozkan pakdil <ozkan.pakdil@gmail.com>
2018-10-03 13:52:19 +0200

    mvn tests fixed for win

--- src/main/java/com/mascix/proxycmd/whois/service/CmdServiceImpl.java
+++ src/main/java/com/mascix/proxycmd/whois/service/CmdServiceImpl.java
@@ -10,28 +10,33 @@ import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class CmdServiceImpl implements CmdService {
-	private static final Logger logger = LoggerFactory.getLogger(CmdServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(CmdServiceImpl.class);
 
-	@Override
-	public String execToString(String ip) throws Exception {
-		String[] cmd = { "/bin/sh", "-c", "torify whois " + ip };
+    @Override
+    public String execToString(String ip) throws Exception {
+        String c1 = "/bin/sh";
+        if (System.getProperty("os.name").toLowerCase().contains("windows"))//check is OS windows
+            return "pass";
+        String[] cmd = {c1, "-c", "torify whois " + ip};
 
-		ProcessBuilder pb = new ProcessBuilder(cmd);
-		Process p = pb.start();
-		p.waitFor();
+        ProcessBuilder pb = new ProcessBuilder(cmd);
+        Process p = pb.start();
+        p.waitFor(60, TimeUnit.SECONDS);
 
-		return IOUtils.toString(p.getInputStream(), "UTF-8");
-	}
+        return IOUtils.toString(p.getInputStream(), "UTF-8");
+    }
 
-	@Override
-	@Scheduled(fixedRate = 5000 * 12)
-	public void printCacheStats() {
-		for (HazelcastInstance ins : Hazelcast.getAllHazelcastInstances()) {
-			logger.info(ins.getName());
-			IMap<Object, Object> map = ins.getMap("whois");
-			logger.info(map.getLocalMapStats().toJson().toString());
-		}
-	}
+    @Override
+    @Scheduled(fixedRate = 5000 * 12)
+    public void printCacheStats() {
+        for (HazelcastInstance ins : Hazelcast.getAllHazelcastInstances()) {
+            logger.info(ins.getName());
+            IMap<Object, Object> map = ins.getMap("whois");
+            logger.info(map.getLocalMapStats().toJson().toString());
+        }
+    }
 }
--- src/test/java/com/mascix/proxycmd/whois/tests/CmdControllerTest.java
+++ src/test/java/com/mascix/proxycmd/whois/tests/CmdControllerTest.java
@@ -3,10 +3,15 @@ package com.mascix.proxycmd.whois.tests;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.mascix.proxycmd.whois.CmdController;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CmdControllerTest {
 	@Autowired
 	CmdController cmdController;
