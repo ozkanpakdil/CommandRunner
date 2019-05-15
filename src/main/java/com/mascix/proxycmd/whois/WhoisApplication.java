3d7b01522b9ded76a44832e150514a794716aff2
ozkanpakdil <ozkan.pakdil@gmail.com>
2019-05-15 07:56:40 +0200

    new caching configuration

--- src/main/java/com/mascix/proxycmd/whois/WhoisApplication.java
+++ src/main/java/com/mascix/proxycmd/whois/WhoisApplication.java
@@ -1,5 +1,9 @@
 package com.mascix.proxycmd.whois;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionPolicy;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.MaxSizeConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
@@ -16,17 +20,28 @@ import com.hazelcast.spring.cache.HazelcastCacheManager;
 @SpringBootApplication
 public class WhoisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WhoisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WhoisApplication.class, args);
+    }
 
-	@Bean
-	CacheManager cacheManager() {
-		return new HazelcastCacheManager(hazelcastInstance());
-	}
+    @Bean
+    CacheManager cacheManager() {
+        return new HazelcastCacheManager(hazelcastInstance());
+    }
 
-	@Bean
-	HazelcastInstance hazelcastInstance() {
-		 return Hazelcast.newHazelcastInstance();
-	}
+    @Bean
+    HazelcastInstance hazelcastInstance() {
+        Config cfg = new Config();
+        MapConfig defaultMapConfig = new MapConfig();
+        defaultMapConfig.setName("default");
+        defaultMapConfig.setTimeToLiveSeconds(60 * 10);
+        defaultMapConfig.setEvictionPolicy(EvictionPolicy.LFU);
+        defaultMapConfig.setMaxIdleSeconds(60 * 10);
+        MaxSizeConfig defaultMaxSizeConfig = new MaxSizeConfig();
+        defaultMaxSizeConfig.setSize(1000);
+        defaultMaxSizeConfig.setMaxSizePolicy(MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE);
+        defaultMapConfig.setMaxSizeConfig(defaultMaxSizeConfig);
+        cfg.addMapConfig(defaultMapConfig);
+        return Hazelcast.newHazelcastInstance(cfg);
+    }
 }
